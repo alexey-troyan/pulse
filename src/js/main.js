2a81a195671d09f736140c3cf19072de4906afd8
alexey-troyan <prime891891@gmail.com>
2020-09-01 21:29:23 +0300

    slider

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.slider__wrap').slick({
+    speed: 1000,
+    adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../img/prev.png" alt="prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../img/next.png" alt="prev"></button>',
+    // autoplay: true,
+    // autoplaySpeed: 3000,
+  });
+});
