69ccb0cb184b64d85f78d370e64a16b3c22003d7
alexey-troyan <prime891891@gmail.com>
2020-09-04 19:49:50 +0300

    ad catalog

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
   $('.slider__wrap').slick({
     speed: 1000,
-    adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="../img/prev.png" alt="prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../img/next.png" alt="prev"></button>',
+    // adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.png" alt="prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/next.png" alt="prev"></button>',
+    // responsive: [
+    //   {
+    //     breakpoint: 992,
+    //     settings: {
+    //       dots: true,
+    //       arrows: false,
+    //     }
+    //   },
+    // ]
     // autoplay: true,
     // autoplaySpeed: 3000,
   });
